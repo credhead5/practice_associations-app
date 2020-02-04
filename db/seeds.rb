5263588bf204ac2b27c265c8c02792b0e03bbe67
christine <credhead89@gmail.com>
2020-02-03 23:27:56 -0500

    create new app making new associations

---
+++ db/seeds.rb
@@ -0,0 +1,13 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# Speaker.create({first_name: "Jason", last_name: "Bateman", email: "jb@aol.com"})
+# Speaker.create({first_name: "Toni", last_name: "Morrison", email: "tm@gmail.com"})
+# Speaker.create({first_name: "Sammy", last_name: "Sosa", email: "batman@yahoo.com.com"})
+Meeting.create({title: "Lets Talk World Series", agenda: "How to be an MVP", location: "Prudential Center", time: "Evening"})
+Meeting.create({title: "Hollywood", agenda: "What it takes to make it", location: "Warner Studio", time: "Morning"})
+Meeting.create({title: "Influence", agenda: "What Happens When you succeed", location: "Prudential Center", time: "Evening"})
